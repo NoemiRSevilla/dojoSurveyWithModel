50cec6d44c06826fc2386a00c8c9f39b134b5b66
irockmywarlock <noemirsevilla@gmail.com>
2020-09-03 19:37:16 -0500

    initial commit

---
+++ Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using dojoSurveyWithModel.Models;
+
+namespace dojoSurveyWithModel.Controllers
+{
+    public class UserController : Controller
+    {
+        [HttpGet("")]
+        public IActionResult Index(string name, string dojo_location, string favorite_language, string comments)
+        {
+            return View();
+        }
+        [HttpPost("survey")]
+        public IActionResult Submission(string name, string dojo_location, string favorite_language, string comments)
+        {
+            Survey newSurvey = new Survey();
+            newSurvey.name = name;
+            newSurvey.dojo_location = dojo_location;
+            newSurvey.favorite_language = favorite_language;
+            newSurvey.comments = comments;
+            return View("Submission", newSurvey);
+        }
+    }
+}
---
+++ Models/Survey.cs
@@ -0,0 +1,10 @@
+namespace dojoSurveyWithModel.Models
+{
+    public class Survey
+    {
+        public string name {get; set;}
+        public string dojo_location { get; set; }
+        public string favorite_language { get; set; }
+        public string comments { get; set; }
+    }
+}
